9aead030d4cc094c521066d785ea1d7d6b9aba5f
akrblt <a.karabulut070@gmail.com>
2023-09-30 22:59:56 +0300

    .

--- src/assets/js/28-dom-btn-delete.js
+++ src/assets/js/28-dom-btn-delete.js
@@ -50,7 +50,7 @@ document.querySelectorAll("#tblStudent tbody tr").forEach((row)=>{
   row.addEventListener("click",(e)=>{
     e.target.closest("tr").classList.toggle("table-warning");
     
-    console.log()
+    
   })
 })
 
